cf3ca05a8ec40424fcf35a7b3d8487b24a06a4ac
Adityanpx <137485127+Adityanpx@users.noreply.github.com>
2025-04-12 16:29:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 document.querySelector(".enterBtn").addEventListener("click", function () {
 	let p = document.querySelector("p");
-	p.innerText= "Entered Metaverse"
+	p.innerHTML= " <h1>Entered Metaverse</h1>"
 });
 	
